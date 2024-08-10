79be9eafdd34026ef83d920932c2ded4f2e05dc1
SUI <numerical43@gmail.com>
2024-08-11 01:41:49 +0900

    [#5] feat : DB연결

---
+++ src/lib/db.tsx
@@ -0,0 +1,24 @@
+import mysql from 'mysql2/promise';
+
+export async function query({ query, values = [] }: { query: string; values?: any[] }) {
+    if (!process.env.DB_HOST || !process.env.DB_USER || !process.env.DB_PASSWORD || !process.env.DB_NAME) {
+        throw new Error('Database configuration is incomplete');
+    }
+
+    const dbConnection = await mysql.createConnection({
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT ? parseInt(process.env.DB_PORT, 10) : 3306, // 기본 MySQL 포트
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+    });
+
+    try {
+        const [results] = await dbConnection.execute(query, values);
+        await dbConnection.end();
+        return results;
+    } catch (error) {
+        console.error('Database error:', error);
+        throw new Error('Database error occurred');
+    }
+}

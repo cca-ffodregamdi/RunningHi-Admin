c2683a01de4195cd7bfbc64f247e6ff4b2446a31
SUI <numerical43@gmail.com>
2024-08-11 21:15:16 +0900

    [#5] delete : 필요없는 부분(파일, 주석, 코드) 삭제

--- src/lib/db.tsx
+++ src/lib/db.tsx
@@ -7,7 +7,7 @@ export async function query({ query, values = [] }: { query: string; values?: an
 
     const dbConnection = await mysql.createConnection({
         host: process.env.DB_HOST,
-        port: process.env.DB_PORT ? parseInt(process.env.DB_PORT, 10) : 3306, // 기본 MySQL 포트
+        port: process.env.DB_PORT ? parseInt(process.env.DB_PORT, 10) : 3306,
         user: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
         database: process.env.DB_NAME,
